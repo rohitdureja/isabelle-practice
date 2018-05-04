214751f734d80b3cd917b5c50dfdf87e764629eb
Rohit Dureja <rohit.dureja@gmail.com>
2018-05-04 00:58:33 -0500

    wip for polynomial

--- Exercises2_4.thy
+++ Exercises2_4.thy
@@ -48,12 +48,49 @@ fun evalp :: "int list \<Rightarrow> int \<Rightarrow> int" where
 "evalp (Cons x Nil) v = x" |
 "evalp (Cons x xs) v = x + v * (evalp xs v)"
 
+(* add coefficients for two lists
+Examples: (addition is from the left)
+[2,3,4] + [4,5] = [6,7,4]
+[5,6] + [3,2,6] = [8,8,6] 
+*)
+fun add_coeff :: "int list \<Rightarrow> int list \<Rightarrow> int list" where
+"add_coeff Nil ys = ys" |
+"add_coeff xs Nil = xs" |
+"add_coeff (x#xs) (y#ys) = (x+y)#(add_coeff xs ys)"
+
+
+lemma evalp_add : "evalp (add_coeff xs ys) x = evalp xs x + evalp ys x"
+  apply(induction xs) 
+   apply(auto)
+
+(* multiply all items in a list with a number
+Example:
+4 * [3,4,2] = [12,16,8]
+*)
+fun mult :: "int \<Rightarrow> int list \<Rightarrow> int list" where
+"mult n Nil = Nil" |
+"mult n (x#xs) = (n*x)#(mult n xs)"
+
+(* multiply coefficients for two lists
+Example:
+[1,2,3]*[4,5] = [4,13,22,15]
+[1,1,1]*[1,1] = [1,2,2,1]
+*)
+fun mult_coeff :: "int list \<Rightarrow> int list \<Rightarrow> int list" where
+"mult_coeff xs Nil = Nil" |
+"mult_coeff Nil ys = Nil" |
+"mult_coeff (x#xs) ys = add_coeff (mult x ys) (0#(mult_coeff xs ys))"
+
 (* coeff function *)
 (* transforms an expression to a polynomial*)
 fun coeff :: "exp \<Rightarrow> int list" where
-"coeff Var = [1]" |
+"coeff Var = [0,1]" |
 "coeff (Const i) = [i]" |
-"coeff (Add e f) = (coeff e)@(coeff f)" |
-"coeff (Mult e f) = (coeff e)@(coeff f)"
+"coeff (Add e f) = add_coeff (coeff e) (coeff f)" |
+"coeff (Mult e f) = mult_coeff (coeff e) (coeff f)"
+
+theorem preservation : "evalp (coeff e) x = eval e x"
+  apply(induction e arbitrary: x)
+  apply(auto)
 
 end
